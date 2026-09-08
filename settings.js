88152d8540b36a6b0fa3b469deecdea06d0eea10
mgbodilefortune52 <mgbodilefortune52@gmail.com>
2026-09-08 14:54:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot‎',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '2347019003666', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+2347019003666', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
